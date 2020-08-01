33e52df8175013bb3d9dc401d17013072056a180
Sha <sh.shahrabi@hotmail.com>
2020-08-01 23:04:39 +0200

    starting implementing a search domain restricion for positon

---
+++ Assets/Scripts/TestSections/SampleOnEdgesTest.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+     
+public class SampleOnEdgesTest : MonoBehaviour
+{
+
+    public Texture        mask;
+
+    private Material      debug_positions_mat;
+
+    private ComputeShader construct_position_domain_compute;
+
+    private ComputeBuffer position_domain_buffer;
+    private ComputeBuffer debug_positions_buffer;
+    private ComputeBuffer positon_domain_arguments_buffer;
+
+    private CommandBuffer command_buffer;
+
+    private int           numberOfPoints = 32768;
+
+    private Camera        cam;
+
+    private int           Construct_Position_Domain_handel;
+    private int           Debug_Position_Domain_handel;
+
+    private int           generation_id;
+
+    void Start()
+    {
+        // -----------------------------------------
+        debug_positions_mat = new Material(Shader.Find("Unlit/pointRenderer"));
+        if (debug_positions_mat) Debug.LogError("No Material found!");
+
+        // -----------------------------------------
+        Construct_Position_Domain_handel = construct_position_domain_compute.FindKernel("CS_Construct_Position_Domain");
+        Debug_Position_Domain_handel     = construct_position_domain_compute.FindKernel("CS_Debug_Position_Domain");
+
+        construct_position_domain_compute.SetTexture(Construct_Position_Domain_handel, "_mask",                   mask);
+        construct_position_domain_compute.SetBuffer (Construct_Position_Domain_handel, "_position_domain_buffer", position_domain_buffer);
+
+        construct_position_domain_compute.SetInt("_image_width",  mask.width);
+        construct_position_domain_compute.SetInt("_image_height", mask.height);
+
+        construct_position_domain_compute.SetBuffer(Debug_Position_Domain_handel, "_R_position_domain_buffer",        position_domain_buffer);
+        construct_position_domain_compute.SetBuffer(Debug_Position_Domain_handel, "_position_domain_argument_buffer", positon_domain_arguments_buffer);
+        construct_position_domain_compute.SetBuffer(Debug_Position_Domain_handel, "_debug_position_buffer",           debug_positions_buffer);
+
+        // -----------------------------------------
+
+        if (mask.width > 1024 || mask.height > 1024) Debug.LogError("image provided is bigger than 1024. This probabaly not what you want");
+
+        position_domain_buffer = new ComputeBuffer(mask.width * mask.height, sizeof(float) * 2, ComputeBufferType.Append);
+        position_domain_buffer.SetCounterValue(0);
+
+        //_____________
+        positon_domain_arguments_buffer = new ComputeBuffer(1, sizeof(int) * 4, ComputeBufferType.IndirectArguments);
+        int[] ini_value = new int[4] { 0, 0, 0, 0 };
+        positon_domain_arguments_buffer.SetData(ini_value);
+
+        //_____________
+
+        debug_positions_buffer = new ComputeBuffer(numberOfPoints, sizeof(float) * 2);
+
+        // -----------------------------------------
+
+        cam = Camera.main;
+        if (!cam) Debug.LogError("No Camera is tagged as main");
+
+        cam.clearFlags = CameraClearFlags.Nothing;
+
+
+        // -----------------------------------------
+
+        construct_position_domain_compute.Dispatch(Construct_Position_Domain_handel, mask.width / 8, mask.height / 8, 1);
+        ComputeBuffer.CopyCount(position_domain_buffer, positon_domain_arguments_buffer, 0);
+
+        // -----------------------------------------
+
+        command_buffer = new CommandBuffer()
+        {
+            name = "constructPositionDomain"
+        };
+
+
+        command_buffer.DispatchCompute(construct_position_domain_compute, Debug_Position_Domain_handel,
+                                       numberOfPoints / 64, 1, 1);
+        command_buffer.DrawProcedural(Matrix4x4.identity, debug_positions_mat, 0, MeshTopology.Points, numberOfPoints);
+    }
+
+
+    void Update()
+    {
+        construct_position_domain_compute.SetInt("_generation_seed", generation_id);
+        generation_id++;
+    }
+}
---
+++ Assets/Shaders/Resources/pointRenderer.shader
@@ -0,0 +1,45 @@
+﻿
+Shader "Unlit/pointRenderer"
+{
+   
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+         ZTest Always Cull Off ZWrite Off
+
+        Pass
+        {
+            CGPROGRAM
+            
+            #pragma target   5.0  
+            #pragma vertex   vert
+            #pragma fragment frag
+
+
+            #include "UnityCG.cginc"
+
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+            };
+
+            uniform StructuredBuffer<float2> buffer;
+
+            v2f vert (uint id : SV_VertexID)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(float4(buffer[id].xy, 0., 1.));
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+               
+                return float4(0.6,0.8,0.1,1.);
+            }
+            ENDCG
+        }
+    }
+}
