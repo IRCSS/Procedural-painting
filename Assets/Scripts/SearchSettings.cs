0d261e662d70779e9d20570ff7b4033c561d516b
Sha <sh.shahrabi@hotmail.com>
2020-08-08 16:11:08 +0200

    started abstracting the settings in scriptable obejcts

---
+++ Assets/Scripts/SearchSettings.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SearchSettings", order = 1)]
+public class SearchSettings : ScriptableObject
+{
+
+    public StageSeries[] series = new StageSeries[1];
+}
